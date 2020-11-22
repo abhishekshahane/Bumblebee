0f785a1422620ebda12da8191ccc4e7acdf42629
Naishadh Patel <aquasimba@live.com>
2020-11-22 11:02:08 -0500

    added command_handler

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const first = require('discord.js');
 const client = new first.Client();
 var dict = require('./dict.jsonc');
 var dicta = require('./dicta.jsonc');
-var xp = {};
+var CommandHandler = require('./commands/commands');
+var dictionary = {
+    users = [],
+    ids = []
+}
+function userObject(id, xp){
+ this.id = id;
+ this.xp = xp; 
+} 
+const prefix = '--';
+
 //Copy paste bot token in empty quotes
-client.login('')
 client.on('ready', ready);
 function ready(){
     console.log("Alright, we are ready!")
     client.user.setActivity("Drinking Water!💧💦")
 }
-//Call is working
+/*/Call is working
 function call(msg){
     var f =0;
     if (msg.channel.name == undefined){
@@ -21,36 +30,33 @@ function call(msg){
         }
     }
     else{
-        if (dict[msg.author.discriminator]>=0){
-            dict[msg.author.discriminator]+=1;
-            xp[msg.author.discriminator]+=Math.floor(Math.random()*(15-10)+10);
+        if (dict[msg.author.id]>=0){
+            dict[msg.author.id]+=1;
+            xp[msg.author.id]+=Math.floor(Math.random()*(15-10)+10);
         }
-        else if(dict.hasOwnProperty(msg.author.discriminator)!=true){
-            xp[msg.author.discriminator] = Math.floor(Math.random() * (15 - 10) + 10);
-            dict[msg.author.discriminator] = 0;
-            dicta[msg.author.discriminator] = msg.author.username;
+        else if(dict.hasOwnProperty(msg.author.id)!=true){
+            xp[msg.author.id] = Math.floor(Math.random() * (15 - 10) + 10);
+            dict[msg.author.id] = 0;
+            dicta[msg.author.id] = msg.author.username;
             //Gets server id from message, and uses that in server name
             var ida = msg.channel.guild.id;
             var serverName = client.guilds.cache.get(`${ida}`).name;
             console.log(serverName)
-            var person = dicta[msg.author.discriminator];
+            var person = dicta[msg.author.id];
             if (!msg.author.bot){
                 msg.author.send(`Welcome to ${serverName}, ${person}!`)
             }
         }
         if (msg.content==='--check'){
-            msg.channel.send("Returning JSON data......");
-            for (var key in dict){
-                msg.channel.send(`${dicta[key]} : ${dict[key]+1}`)
-            }
+            
         }
         else if(msg.content.startsWith("!joke"){
-                //Put your code here
+            //Put your code here
         }
         else if(msg.content.startsWith("--xp")){
             var strsplit = msg.content.split(" ");
             if (strsplit.length===2){
-                var get = msg.mentions.users.first().discriminator;
+                var get = msg.mentions.users.first().id;
                 if (dicta[get]===undefined){
                     msg.channel.send("This user has not said anything yet, so their XP is 0.")
                 }
@@ -65,5 +71,31 @@ function call(msg){
     }
     
 }
+*/
+function main(msg){
+    if (msg.content.startsWith(prefix)){
+        let args = msg.content.substring(1).split('');
+        switch (args[0]){
+            case 'xp' :
+                CommandHandler.xp(msg,dictionary,args);
+            break;
+            case 'check': 
+                CommandHandler.check(msg,dictionary,args);
+            break;
+            case 'joke':
+                CommandHandler.joke(msg,dictionary,args);
+            default:
+                msg.channel.send("That is not a valid command.")
 
-client.on('message', call);
+
+        }
+
+    } else {
+        return;
+    }
+}
+
+
+client.on('message', main);
+
+client.login('Nzc3Nzc5MjcyOTk4NzE1NDEz.X7IZZQ.a-1jFit4CoRnmvJrAFZA6ZXgZyk');
---
+++ commands/commands.js
@@ -0,0 +1,8 @@
+exports.check = function check(msg,dictionary){
+    
+msg.channel.send("Returning JSON data......");
+for (var key in dict){
+    msg.channel.send(`${dicta[key]} : ${dict[key]+1}`)
+}};
+exports.xp = function xp(msg,dictionary){};
+exports.joke = function joke(msg,dictionary){}; 
