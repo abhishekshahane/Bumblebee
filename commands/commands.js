161a224b7b3b10d5c086b78d12b170e89d25cf8d
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2020-11-23 11:34:28 -0500

    added xp command

--- commands/commands.js
+++ commands/commands.js
@@ -1,6 +1,6 @@
-exports.check = function check(msg,dictionary,intent){
+exports.check = function check(msg,dictionary,args, intent){
 if(dictionary.ids.contains(msg.author.id)){
-    let userObject = finduserinDB();
+    let userObject = finduserinDB(dictionary,msg.author.id);
     if(intent="message"){
         msg.channel.send('User is in database:');
         msg.channel.send('User xp is '+userObject.xp);
@@ -19,7 +19,28 @@ if(dictionary.ids.contains(msg.author.id)){
 }   
 
 };
-exports.xp = function xp(msg,dictionary){};
+exports.xp = function xp(msg,dictionary, args){
+    if(args[1]){
+        try {
+            
+            var mentioned = msg.mentions.users.first().id;
+            let userObject = finduserinDB(dictionary,mentioned);
+            if (dictionary.userObject.xp == 0){
+                msg.channel.send("This user has not said anything yet, so their XP is 0.")
+            }
+            else{
+                msg.channel.send(msg.mentions.users.first().tag+"'s xp is: "+dictionary.userObject.xp);
+            }
+        } catch (error) {
+           if (error){
+            msg.channel.send('The syntax of your command is incorrect. Please follow this format --xp @[USER]')
+           }
+        }
+        }
+        else{
+        msg.channel.send("The syntax of your command is incorrect. Please follow the format --xp @[USER].")
+    }
+};
 exports.joke = function joke(msg,dictionary){}; 
 
 function finduserinDB(dictionary,id){
