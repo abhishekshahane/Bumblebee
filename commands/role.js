654e73173132bef02cb853bf51ab36ec63ba6381
Glitch (bumblebee-taken) <none>
2020-11-23 09:04:08 +0000

    Updated --role

--- commands/role.js
+++ commands/role.js
@@ -1,6 +1,3 @@
-//change anything you dont like lol
-//alright cool
-
 module.exports = {
   name: "role",
   run: (message, client) => {
@@ -8,39 +5,39 @@ module.exports = {
     if (!message.member.hasPermission('MANAGE_ROLES')) {
       return message.channel.send({embed: {
         description: "You do not have the `MANAGE_ROLES` permission to give roles to users",
-        color: "00FF00"
+        color: "FF0000"
       }})
     }
     if (!message.guild.me.hasPermission('MANAGE_ROLES')) {
       return message.channel.send({embed: {
         description: "I do not have the `MANAGE_ROLES` permission to give roles to users",
-        color: "00FF00"
+        color: "FF0000"
       }})
     }
     if (!toGive) {
       return message.channel.send({embed: {
         description: "Mention a user for me to give a role to!",
-        color: "00FF00"
+        color: "FF0000"
       }})
     }
-    let args = message.content.split(' ').slice(1) // --role @USER [Color] [role name]
+    let args = message.content.split(' ').slice(1)
     console.log(args)
     if (args[0] != `<@!${toGive.id}>`) {
       return message.channel.send({embed: {
         description: "The correct format is `--role @USER [role name] [Color]`",
-        color: "00FF00"
+        color: "FF0000"
       }})
     }
     if (!isHexColor(args[1].split('#')[1])) {
       return message.channel.send({embed: {
         description: "The color must be in hexadecimal form!",
-        color: "00FF00"
+        color: "FF0000"
       }})
     }
     if (!args[2]) {
       return message.channel.send({embed: {
         description: "Please provide a name for the role!",
-        color: "00FF00"
+        color: "FF0000"
       }})
     }
     message.guild.roles.create({
@@ -58,14 +55,14 @@ module.exports = {
         console.log(e)
         return message.channel.send({embed: {
           description: "An error occured when I was attempting to give the role to " + `<@${toGive.id}>`,
-          color: "00FF00"
+          color: "FF0000"
         }})
       })
     }).catch(e => {
       console.log(e)
       return message.channel.send({embed: {
         description: "An error occured when I was attempting to create the role",
-        color: "00FF00"
+        color: "FF0000"
       }})
     })
   }
