9eb33d341d095888cdb4a71b9f1c2833d09473c7
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-11-25 14:14:30 +0530

    Create prune.js

---
+++ commands/prune.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+//Keep in mind that the bot has to have admin perms for this to work!
+module.exports = {
+    name: "prune",
+    usage: "!purge [n>1 && n<100]",
+    aliases: ["purge", "bulkdelete"],
+    run: async (message, client, args) => {
+        if (!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) { //check for perms
+            return message.channel.send({embed: {color: "#008000", description: "You don't have access to this command."}})
+        };
+      //convert into an integer
+        const amount = parseInt(args[0]);
+  
+        if (isNaN(amount)) { // if amount is not a number
+            console.log(amount);
+            console.log(args[0]);
+            return message.reply({embed: {color: "#008000", description: "Please input a valid number."}}) // error
+        } else if (amount <1 || amount > 100) { // if amount is less then 1 or more than 100(limit) 
+            return message.reply({embed: {color: "#008000", description: "Please input a number between 1 and 100."}}) // error
+        } else { 
+            //bulk delete amount
+            return message.channel.bulkDelete(amount, true).catch(err => {// if there is an error
+            console.error(err);
+            return message.reply({embed: {color: "#008000", description: "There was an error, please try again later."}}) // error
+            });
+        }
+  }
+};
