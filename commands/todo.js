fe280a03ccf0f6d6eeeb71869de7f7672cfc5f85
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-11-27 18:03:53 +0530

    changed todo a bit

--- commands/todo.js
+++ commands/todo.js
@@ -2,7 +2,7 @@ const db = require('quick.db')//we'll need quick.db
 
 module.exports={
   name: "todo",
-  usage: "!todo add [STUFF] or !todo delete [STUFF] or !todo",
+  usage: "!todo add [STUFF] or !todo delete [number of the todo] or !todo",
   run: async(message, client) => {
     //so first, we need to add this in the db
     //done, now for checking what the args is 
@@ -20,7 +20,7 @@ module.exports={
       arr.push(args[1])
       await db.set(`User${message.author.id}.todo`, arr)
       let embed = {
-        description: `We've added your todo to your todos list, ${message.author.username}!`,
+        description: `We've added that todo to your todos list, ${message.author.username}!`,
         color: "#808000"//olive in hexcode
       }
       return message.channel.send({
@@ -29,15 +29,15 @@ module.exports={
     }
     else if(strsplit[1]==='delete'){
       //get the persons todo
-      const args = message.content.split("delete ")
+      const args = message.content.split("delete")
       //removes the add, so the todo will be args[1]
-      const todo = args[1];
+      const todo = parseInt(args[1]);
       let arr = await db.get(`User${message.author.id}.todo`)
       if (!arr) {
         await db.set(`User${message.author.id}.todo`, [])
         arr = await db.get(`User${message.author.id}.todo`) 
       }
-      let i = arr.indexOf(todo)
+      let i = arr[todo-1];
       if (i == -1) {
         let embed = {
           description: `We could not find "${todo}" in your todo list`, 
@@ -45,7 +45,7 @@ module.exports={
         }
         return message.channel.send({embed: embed})
       }
-      arr.splice(i, 1)
+      arr.splice(todo-1, 1)
       await db.set(`User${message.author.id}.todo`, arr)
       let embed = {
         description: `We've deleted your todo from your todos list ${message.author.username}!`,
