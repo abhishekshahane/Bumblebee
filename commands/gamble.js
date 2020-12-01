9b545ea73a08e0d52e55af8e0f1b83ff1f4ad2cb
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-12-01 17:53:35 +0530

    🎲added stuff to gamble

--- commands/gamble.js
+++ commands/gamble.js
@@ -1,4 +1,3 @@
-//Yet to finish this
 const db = require('quick.db')
 const Discord = require('discord.js');
 const random = require("random");
@@ -7,48 +6,76 @@ module.exports = {
   usage: "!gamble [amount > 10]",
   run: async(msg, client)=> {
     let args = msg.content.split(' ')
-    let f = {
-      description: "Oops, please follow the format for gamble(!gamble [amount > 10]).",
-      color: "#FF0000"//red in hex code
-    }
-    if (args.length===1 || parseInt(args[1])<11){
-      const embed = new Discord.MessageEmbed()
-      .setTitle(f.description)
-      .setColor(f.color)
-      msg.channel.send(embed)
-    }
-    else if(args.length>2){
-      const embed = new Discord.MessageEmbed()
-      .setTitle(f.description)
-      .setColor(f.color)
-      msg.channel.send(embed)
-    }
-    else{
-      let diceToRoll = [[0,1,2,3,4,5,6],[0,1,2,3,4,5,6],[0,1,2,3,4,5,6], [0,1,2,3,4,5,6],[0,1,2,3,4,5,6],[0,1,2,3,4,5,6],[0,1,2,3,4,5,6]]
-      let newArr = []
-      for (var i=0;i<diceToRoll.length;i++){
-        let curr = diceToRoll[i][random.int(0,6)]
-        newArr.push(curr)
-        
+      let f = {
+        description: "Oops, please follow the format for gamble(!gamble [1500 > amount > 10]).",
+        color: "#FF0000"//red in hex code
       }
-      console.log(newArr)
-      let Check = newArr[0];
-      let bool = true
-      for (var j=1;j<newArr.length;j++){
-        if (newArr[j]!=Check){
-          bool=false
+      if (args.length===1 || parseInt(args[1])<11|| parseInt(args[1]) > 1500){
+        const embed = new Discord.MessageEmbed()
+        .setTitle(f.description)
+        .setColor(f.color)
+        return msg.channel.send(embed)
+      }
+      else if(args.length>2){
+        const embed = new Discord.MessageEmbed()
+        .setTitle(f.description)
+        .setColor(f.color)
+        return msg.channel.send(embed)
+      }
+    let msgtodelete = await msg.channel.send(
+      {embed: {
+      description: "Rolling the dice :game_die:",
+      color: "#008000"
+    }})
+    setTimeout(() => {
+      //you can use foreach for this, but i wanted to make this more logical:)
+        let diceToRoll = [[0,1,2,3,4,5,6],[0,1,2,3,4,5,6],[0,1,2,3,4,5,6], [0,1,2,3,4,5,6],[0,1,2,3,4,5,6],[0,1,2,3,4,5,6],[0,1,2,3,4,5,6]]
+        let newArr = []
+        for (var i=0;i<diceToRoll.length;i++){
+          let curr = diceToRoll[i][random.int(0,6)]
+          newArr.push(curr)
+
+        }
+        console.log(newArr)
+        let Check = random.int(0,6);
+        let bool = true
+        for (var j=1;j<newArr.length;j++){
+          if (newArr[j]!=Check){
+            bool=false
+            break
+          }
+          else{
+            bool=true
+            //same as it was
+          }
+        }
+        if (bool==false){
+          //choose a number between the max and min
+          let num = random.int(2, parseInt(args[1]))
+          db.subtract(`User${msg.author.id}.coins`, num)
+          let embed1 = {
+            description: `
+            ***LOL*** you didn't roll straight dice unfortunately so you lost ${num} coins lol. Here are your stats:
+            `,
+            color: "#FF0000",
+            fields: [
+              {
+                name: 'You rolled a\n',
+                value: `${Check}`
+              }
+
+            ]
+          }
+          msgtodelete.delete()
+          msg.channel.send({
+            embed: embed1
+          })
+
         }
         else{
-          bool=true
-          //same as it was
+          let num = random.int(2, parseInt(args[1]))
+          let otherMsga = `Nice ${msg.author.username}! You've won ${num} coins`
         }
-      }
-      if (bool==false){
-        msg.channel.send("BYE")
-      }
-      else{
-        msg.channel.send("HI")
-      }
-    }
+    }, 10000)
   }
 }
