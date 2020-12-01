9604b79372d42b27445a61e3e13cbe30159f832b
NightCry1 <66012697+NightCry1@users.noreply.github.com>
2020-12-01 22:08:22 +0800

    💰Added daily.js
    ezpz file made by Haru#0001 😎

---
+++ commands/daily.js
@@ -0,0 +1,18 @@
+const db = require('quick.db')
+const alreadyClaimed = new Set()
+const random = require('random')
+
+module.exports = {
+  name: "daily",
+  usage: "!daily",
+  run: async(msg, client) =>{
+    if (alreadyClaimed.has(msg.author.id)) return msg.channel.send("already claimed daily") //if already claimed daily
+    let coins = db.get(`User${msg.author.id}.coins`) // get user's balance
+    let amt = (1/random.int(10,20))*coins//amount of coins to give
+    await db.add(`User${msg.author.id}.coins`, amt) // add amount to balance
+    alreadyClaimed.add(msg.author.id) // add user to the Set
+    setTimeout(() => {
+      alreadyClaimed.delete(msg.author.id) //after 24hrs remove user from Set
+    }, 8.64e+7)
+  }
+}
