843418985939a10b512efb3d295b898872fec86d
NightCry1 <66012697+NightCry1@users.noreply.github.com>
2020-12-01 22:10:39 +0800

    Update daily.js
    Forgotten confirmation message 🤦‍♂️

--- commands/daily.js
+++ commands/daily.js
@@ -10,6 +10,7 @@ module.exports = {
     let coins = db.get(`User${msg.author.id}.coins`) // get user's balance
     let amt = (1/random.int(10,20))*coins//amount of coins to give
     await db.add(`User${msg.author.id}.coins`, amt) // add amount to balance
+    msg.channel.send("Added " + amt + " coins") // send confirmation message
     alreadyClaimed.add(msg.author.id) // add user to the Set
     setTimeout(() => {
       alreadyClaimed.delete(msg.author.id) //after 24hrs remove user from Set
