e19617644b50a598341313ace8dc766c773dd310
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-12-24 20:51:35 +0530

    Added constants for shop and buy

---
+++ consts/constants.js
@@ -0,0 +1,40 @@
+var thingsToBuy = [
+      {
+        name: "An Apple",
+        price: 50,
+        description: "A single, green apple.",
+        emoji: "🍏",
+        percentageOfGrowth: 10
+      },
+      {
+        name: "Fruit Juice",
+        price: 70,
+        description: "Some nice fruit juice.",
+        emoji: "🧃",
+        percentageOfGrowth: 11
+      }, 
+      {
+        name: "Eggs",
+        price: 180,
+        description: "A carton of white eggs.",
+        emoji: "🥚",
+        percentageOfGrowth: 20
+      }, 
+      {
+        name: "Bread",
+        price: 230,
+        description: "A loaf of brown bread.",
+        emoji: "🍞",
+        percentageOfGrowth: 21
+      },
+      {
+        name: "Fish",
+        price: 300,
+        description: "A fresh-water fish, best enjoyed roasted",
+        emoji: "🐟",
+        percentageOfGrowth: 30
+      }
+];
+module.exports = {
+  thingsToBuy
+}
