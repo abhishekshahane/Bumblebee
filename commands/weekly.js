45a9d7d9cc68a6cb55c4b5480f1984de75c2be2f
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-01-04 21:37:50 +0530

    feat: fixed discord error

--- commands/weekly.js
+++ commands/weekly.js
@@ -2,15 +2,18 @@ const db = require('quick.db')
 module.exports = {
   name: "weekly",
   usage: "!weekly",
-  run: async function(msg, async){
-    //TO FIX
-    var a = db.get(`User${msg.author.id}.stocks`)
+  run: async function(msg, client){
+
+    let a = db.get(`User${msg.author.id}.stocks`)
     if (typeof a === 'undefined' || a.length===0){
       let embed = {
         color: "#FF0000",
         description: "You haven't purchased any stocks, would you like to buy one?"
       }
-      msg.channel.send(embed)
+      msg.channel.send({embed: embed})
+    }
+    else{
+      //code
     }
   }
 }
