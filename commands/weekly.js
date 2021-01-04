6a0370cabb904b2fbd9ba04ae8d6e3a868cea989
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-01-04 22:04:35 +0530

    feat: added description

--- commands/weekly.js
+++ commands/weekly.js
@@ -1,9 +1,13 @@
 const db = require('quick.db')
+const imports = require('../consts/constants.js')
+
 module.exports = {
   name: "weekly",
   usage: "!weekly",
+  description: "Redeem money from your weekly stock purchases!",
   run: async function(msg, client){
-
+    //we need to get this to work first, discord error or smth
+    
     let a = db.get(`User${msg.author.id}.stocks`)
     if (typeof a === 'undefined' || a.length===0){
       let embed = {
@@ -13,7 +17,12 @@ module.exports = {
       msg.channel.send({embed: embed})
     }
     else{
-      //code
+      //ok
+      let percentageOfGrowth = [];
+      for (let i = 0; i < imports.thingsToBuy.length; i++) {
+        percentageOfGrowth.push(imports.thingsToBuy[i].percentageOfGrowth)
+      }
     }
+    
   }
 }
