69b911fbf0d6ce95bfb8913070c743ebb73c64c5
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-01-05 10:11:57 +0530

    feat: added description

--- commands/joke.js
+++ commands/joke.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch');
 
 module.exports = {
   name: "joke",
+  description: "Get a funny joke with this!",
+  usage: "!joke",
   run: async (msg, client) => {
     let result = await fetch('https://official-joke-api.appspot.com/random_joke')
     
