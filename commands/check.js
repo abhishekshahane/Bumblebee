d445f8576d6f17782c1235ad26a2b7b00f3b2d66
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-01-05 10:21:24 +0530

    feat: added description

--- commands/check.js
+++ commands/check.js
@@ -1,9 +1,31 @@
-const db = require('quick.db')
+const db = require("quick.db");
 
 module.exports = {
   name: "check",
-  run: (msg, client) => {
-    let messagesSent = db.get(`User${msg.author.id}.messages`)
-    msg.channel.send(messagesSent)
+  description: "Check how many messages someone has sent on the server with this!",
+  usage: "!check [@user]",
+  run: async (msg, client) => {
+    let strsplit = msg.content.split(" ");
+    if (strsplit.length === 1) {
+      return msg.channel.send({
+        embed: {
+          description: "Oops, please follow the format !check [@USER]",
+          color: "#FF0000" //red for alert
+        }
+      });
+    }
+
+    let firstMentionID = msg.mentions.members.first().id;
+    if (!firstMentionID) {
+      return msg.channel.send({
+        embed: {
+          description: "Oops, please follow the format !check [@USER]",
+          color: "#FF0000" //red for alert
+        }
+      });
+    }
+    //first mention
+    let messagesSent = db.get(`User${firstMentionID}.messages`);
+    msg.channel.send(messagesSent);
   }
-}
+};
