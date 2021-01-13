024e701c9c9fad8e11bb462e28c4579fa4ca13d1
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-01-13 10:27:00 +0530

    fix: caught errors

--- commands/coins.js
+++ commands/coins.js
@@ -1,18 +1,22 @@
-const db = require("quick.db");
+const db = require('quick.db')
 module.exports = {
   name: "coins",
   description: "Get your coin number with this!",
   usage: "!coins [USER MENTION]",
   run: async (msg, client) => {
-    let a = msg.mentions.members.first().user.username;
-    let getcoins = msg.mentions.members.first().id;
-    let coins = Math.floor(db.get(`User${getcoins}.coins`));
-    var embed = {
-      color: "#fafafa",
-      description: `Your coins are ${coins}, ${a}!`
-    };
-    return msg.channel.send({
-      embed: embed
-    });
+    var strsplit = msg.content.split(" ")
+    if (strsplit.length===2){
+      let a = msg.mentions.members.first().user.username;
+      let getcoins = msg.mentions.members.first().id;
+      let coins = Math.floor(db.get(`User${getcoins}.coins`));
+      var embed = {
+        color: "#fafafa",
+        description: `You have ${coins}, ${a}!`
+      };
+      return msg.channel.send({
+        embed: embed
+      });
+    }
+    
   }
 };
