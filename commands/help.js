d73eb7899005b0d6bf4bc4d9a654d82e651f82a4
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-01-28 10:26:17 +0530

    up: fixed bugs, made it into a page format

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,45 @@
 module.exports = {
   name: "help",
-  usage: "!help [command]",
+  usage: "!help [page]",
   description: "Get help with this!",
   run: async (msg, client) => {
-    let text = ""
-    await client.commands.forEach(command => {
-      text += `***${command.name}:***` + "\n"+"\n\tusage:"+" " + command.usage +"\n" + "\n\tdescription: " + command.description + "\n\n"
-    })
-    let embed = {
-      title: "HELP",
-      description: text,
-      color: "#00ff00"
+    var splitit = msg.content.split(" ")
+    if (splitit.length!=2 || msg.content == '!help'){
+      let embed = {
+        color: "#FAFAFA",
+        description: "An invalid message was sent. Follow the format help [page]"
+      }
+      msg.channel.send({embed: embed})
     }
-    msg.channel.send({embed: embed})
+    else{
+      let text = ""
+      var count=0;
+      await client.commands.forEach(command => {
+        text += `***${command.name}:***` + "\n"+"\n\tusage:"+" " + command.usage +"\n" + "\n\tdescription: " + command.description + "\n\n";
+        count++;
+        if (count%5==0){
+          text+= "--"
+        }
+      })
+      var splited = text.split('--')
+      var pagenum = parseInt(splitit[1])
+      if (pagenum>=4){
+        let embed={
+          color: "#FAFAFA",
+          description: "Invalid index provided."
+        }
+        msg.channel.send({embed:embed})
+      }
+      else{
+        let embed = {
+          title: "HELP",
+          description: splited[pagenum],
+          color: "#00ff00"
+        }
+        msg.channel.send(
+          {embed: embed}
+        )
+      }
+    }
   }
 }
