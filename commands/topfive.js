bb6d04c1a24d25c913ceeb9d39f198e6ebbe7d07
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-02-05 21:43:18 +0530

    changed old stuff

--- commands/topfive.js
+++ commands/topfive.js
@@ -1,5 +1,5 @@
-const db = require('quick.db');
-const Discord = require('discord.js');
+const db = require("quick.db");
+const Discord = require("discord.js");
 
 module.exports = {
   name: "topfive",
@@ -7,29 +7,32 @@ module.exports = {
   description: "Get the top 5 people with the highest xp in your server!",
   aliases: ["top", "leaderboard", "lb"],
   run: async (msg, client) => {
-    let all = db.all() //grab everything in the db
-    let xpUsers = []
+    let all = db.all(); //grab everything in the db
+    let xpUsers = [];
     await all.forEach(user => {
-      if (!user.ID.startsWith("User")) return; 
+      if (!user.ID.startsWith("User")) return;
       //push only necessary objects from the db into a different array
-      xpUsers.push(user)
-    })
-    await xpUsers.sort((a, b) => a .xp - b.xp); // sort from highest xp to lowest xp
+      xpUsers.push(user);
+    });
+    await xpUsers.sort((a, b) => a.xp - b.xp); // sort from highest xp to lowest xp
     let embed = new Discord.MessageEmbed() //create embed
-    .setTitle("TOP FIVE")
-    .setColor("#00FF00")
+      .setTitle("TOP FIVE")
+      .setColor("#00FF00");
     let i = 0; //index number
     await xpUsers.forEach(xp => {
-      i++ //add 1 to the index number
-      embed.addField(`${i}. ${getUser(i)}`, "`level: "+xp.data.level+"` `xp: "+xp.data.xp+"`")//addField to the embed
-    })
-    msg.channel.send(embed)// send embed
-    
-    
-    
+      if (getUser(i + 1) != "Someone unknown to us!") {
+        i++;
+        embed.addField(
+          `${i}. ${getUser(i)}`,
+          "`level: " + xp.data.level + "` `xp: " + xp.data.xp + "`"
+        ); //addField to the embed
+      }
+    });
+    msg.channel.send(embed); // send embed
+
     function getUser(num) {
-      let userid = xpUsers[num - 1].ID.split("User")[1] //get a user's id from the db
-      let name = msg.guild.members.cache.get(userid) //find it in the bot
+      let userid = xpUsers[num - 1].ID.split("User")[1]; //get a user's id from the db
+      let name = msg.guild.members.cache.get(userid); //find it in the bot
       if (!name) {
         return "Someone unknown to us!"; //if cant find the user, name will be "Unknown"
       } else {
@@ -37,6 +40,6 @@ module.exports = {
       }
     }
   }
-}
+};
 
 
